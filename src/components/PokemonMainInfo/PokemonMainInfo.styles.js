b2f6c5363214db8ad908ea157b3e29221f5fc719
Esteban <esteb.gtz.mtz@hotmail.com>
2021-04-30 14:07:06 -0500

    remove unusued component list name on index

--- src/components/PokemonMainInfo/PokemonMainInfo.styles.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const NameContainer = styled.div `
-    display: flex; 
-    justify-content: center;
-    width: 7%;
-    height: 90%;
-    background-color: purple;
-`
